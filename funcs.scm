731a9849d24b233c999fbffd5007d94ef97d1bba
imamuray <imauray1313@gmail.com>
2019-11-18 03:50:58 +0900

    ch04 =まで

--- funcs.scm
+++ funcs.scm
@@ -97,3 +97,66 @@
             (cond ((eq? (car lat) old)
                     (cons new (multisubst new old (cdr lat))))
                   (else (cons (car lat) (multisubst new old (cdr lat)))))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define <+>
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (<+> n (sub1 m)))))))
+
+(define <->
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (sub1 (<-> n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (<+> (car tup) (addtup (cdr tup)))))))
+
+(define <*>
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (<+> n (<*> n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((and (null? tup1) (null? tup2) '()))
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else
+        (cons (<+> (car tup1) (car tup2))
+              (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define >
+  (lambda (n m)
+  (cond
+    ((zero? n) #f)
+    ((zero? m) #t)
+    (else (> (sub1 n) (sub1 m))))))
+
+(define <
+  (lambda (n m)
+  (cond
+    ((zero? m) #f)
+    ((zero? n) #t)
+    (else (< (sub1 n) (sub1 m))))))
+
+(define =
+  (lambda (n m)
+    (cond
+      ((> n m) #f)
+      ((< n m) #f)
+      (else #t))))
