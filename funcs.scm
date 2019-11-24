f171beb6b6780c2aea74275f2a720069397c8f19
imamuray <imauray1313@gmail.com>
2019-11-24 13:22:47 +0900

    ch05 完了

--- funcs.scm
+++ funcs.scm
@@ -17,11 +17,11 @@
                 (member? a (cdr lat)))))))
 
 (define rember
-  (lambda (a lat)
+  (lambda (s l)
     (cond
-      ((null? lat) '())
-      ((eq? (car lat) a) (cdr lat))
-      (else (cons (car lat) (rember a (cdr lat)))))))
+      ((null? l) '())
+      ((equal? (car l) s) (cdr l))
+      (else (cons (car l) (rember s (cdr l)))))))
 
 (define firsts
   (lambda (l)
@@ -268,3 +268,52 @@
           ((eq? (car l) a) (add1 (occur* a (cdr l))))
           (else (occur* a (cdr l)))))
       (else (<+> (occur* a (car l)) (occur* a (cdr l)))))))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+          (else (cons (car l) (subst* new old (cdr l))))))
+      (else (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) old) (cons new (cons old (insertL* new old (cdr l)))))
+          (else (cons (car l) (insertL* new old (cdr l))))))
+      (else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      ((atom? (car l))
+        (or (eq? (car l) a) (member* a (cdr l))))
+      (else (or (member* a (car l)) (member* a (cdr l)))))))
+
+(define leftmost
+  (lambda (l)
+    (cond
+      ((atom? (car l)) (car l))
+      (else (leftmost (car l))))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      (else
+        (and (equal? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (s1 s2)
+    (cond
+      ((and (atom? s1) (atom? s2)) (eqan? s1 s2))
+      ((or (atom? s1) (atom? s2)) #f)
+      (else (eqlist? s1 s2)))))
